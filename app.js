b66663999821b30c4131551f1b4c7fc71a9656f5
kiara <yvalenti12@gmail.com>
2023-03-05 11:16:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function id(a) {
+    return document.getElementById(a)
+}
+if (id("RMA")){
+    id("RMA").addEventListener("click", ()=> {
+        names ="yomira"
+        mails ="yriosbarrient@gmail.com"
+        paises="peru"
+        return paises
+    })
+    id("nombre").innerHTML=paises
+}
